702f76844593e557537a210c44846172a3e12f0e
minhnnguyen003 <minhnnguyen003@gmail.com>
2021-06-05 11:43:20 +0700

    Xong phần Upload-game.html

--- js/gameUploadReader.js
+++ js/gameUploadReader.js
@@ -1,14 +1,55 @@
-$folderUpload = document.querySelector('#folder-picker')
+//getElement
 
-$folderUpload.addEventListener("change", function(event) {
-    let output = document.getElementById('listing');
-    let files = event.target.files;
-    
-    for(let i = 0; i < files.length; i++)
+var $uploader = document.getElementById('uploader');
+var $fileUploadbutton = document.getElementById('folder-picker');
+var $gameName = document.getElementById('game-name');
+var $gameUploader = document.querySelectorAll('.game-uploader');
+
+$gameName.addEventListener('input', () => {
+    if($gameName.value == '')
     {
-        let item = document.createElement('li');
-        item.innerHTML = files[i].webkitRelativePath;
-        output.appendChild(item);
-    };
-    console.log(files.length);
-}, false);
+        for(let i of $gameUploader) 
+        {
+            i.style.display = 'none';    
+        }
+    }
+    else
+    {
+        for(let i of $gameUploader) 
+        {
+            i.style.display = 'block';    
+        }
+    }
+})
+
+$fileUploadbutton.addEventListener('change', function () {
+
+    //Get File
+    var files = event.target.files;
+
+    for(let file of files)
+    {
+        //Create a storage Ref
+        let storageRef = firebase.storage().ref(file.webkitRelativePath);
+    
+        //Upload File
+        let task = storageRef.put(file);
+        // console.log(file.webkitRelativePath);
+            //Update progress bar
+        task.on('stage changed', 
+
+            function progress(snapshot) {
+                var percentage = (snapshot.bytesTransferred / snapshot.totalBytes)*100
+                $uploader.value = percentage;
+            },
+
+            function error(err) {
+
+            },
+
+            function complete() {
+
+            },
+        )
+    }
+});
